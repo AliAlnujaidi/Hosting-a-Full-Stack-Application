89148328db5ba596fd9415827d08d60a2aa7c9a3
Ali Alnujaidi <Ali.Alnujaidi4@gmail.com>
2023-09-29 16:05:33 +0300

    ..

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,8 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=database-1.cra1fxts14cl.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=postgres.cra1fxts14cl.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
+export POSTGRES_PORT=5432
 export AWS_BUCKET=arn:aws:s3:::alnujaidi-udacity
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
